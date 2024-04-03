f956956806ad83e635d9f421ab7bb7c148d9f0f7
Sahilbhanushali2354 <sahilbhanushali@gmail.com>
2024-04-03 18:12:34 +0530

    Latest

--- src/pages/Feedback.page.tsx
+++ src/pages/Feedback.page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Button, Radio, Spin, message } from "antd";
 import styled from "styled-components";
 import { FieldValueDTO } from "../types/input.type";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { CurrentActiveFeedbackAtom } from "../store/atom/atom.store";
 import { useRecoilValue } from "recoil";
 
@@ -22,13 +22,21 @@ const FeedbackItem = styled.div`
 
 const Feedback = () => {
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const [formValue, setFormValue] = useState([]);
   const [loader, setLoader] = useState(false);
   const [fieldValue, setFieldValue] = useState({});
   const currentActiveFeedback = useRecoilValue(CurrentActiveFeedbackAtom);
   const [feedbackResponse, setFeedbackResponse] = useState({});
   useEffect(() => {
+    console.log(currentActiveFeedback);
+
+    if (
+      !currentActiveFeedback.id ||
+      !currentActiveFeedback.selectedParticipant
+    ) {
+      return navigate("/activefeedbackforms");
+    }
     getFeedbackForm();
   }, [id]);
 
@@ -67,12 +75,16 @@ const Feedback = () => {
     //   return;
     // }
 
-    for (const [question, answer] in fieldValue) {
-      if (!fieldValue[answer] === "") {
-        message.error("Please select an option for each question.");
-        return;
-      }
-    }
+console.log('----------------',fieldValue);
+console.log('-------formValue',formValue);
+
+
+    // for (const [question, answer] in fieldValue) {
+    //   if (!fieldValue[answer] === "") {
+    //     message.error("Please select an option for each question.");
+    //     return;
+    //   }
+    // }
 
     let totalPoints = 0;
 
@@ -112,61 +124,64 @@ const Feedback = () => {
   };
   return (
     <Spin spinning={!loader} style={{ marginTop: "100px" }}>
-      <div style={{}}>
-        <div>
-          <h2>{`${currentActiveFeedback.selectedParticipant.selectedCompetition.cname} Feedback Form`}</h2>
-        </div>
-        <div>
-          <div>
-            <b>Presenter's Name</b> :{" "}
-            {
-              currentActiveFeedback.selectedParticipant.activeSelectedPartcipant
-                .uname
-            }
+      {loader && (
+        <>
+          <div style={{}}>
+            <div>
+              <h2>{`${currentActiveFeedback.selectedParticipant.selectedCompetition.cname} Feedback Form`}</h2>
+            </div>
+            <div>
+              <div>
+                <b>Presenter's Name</b> :{" "}
+                {
+                  currentActiveFeedback.selectedParticipant
+                    .activeSelectedPartcipant.uname
+                }
+              </div>
+            </div>
+            <div style={{ padding: "5px" }}></div>
+            <div>
+              We value your honest feedback to ensure a fair evaluation of the
+              presenter's performance. Your response to each question will help
+              us gauge how effectively the presenter conveyed their message
+              through their speech. Thank you for taking the time! We value your
+              honest feedback to ensure a fair evaluation of the presenter's
+              performance. Your response to each question will help us gauge how
+              effectively the presenter conveyed their message through their
+              speech. Than k you for taking the time!
+            </div>
           </div>
-        </div>
-        <div style={{ padding: "5px" }}></div>
-        <div>
-          We value your honest feedback to ensure a fair evaluation of the
-          presenter's performance. Your response to each question will help us
-          gauge how effectively the presenter conveyed their message through
-          their speech. Thank you for taking the time! We value your honest
-          feedback to ensure a fair evaluation of the presenter's performance.
-          Your response to each question will help us gauge how effectively the
-          presenter conveyed their message through their speech. Than k you for
-          taking the time!
-        </div>
-      </div>
-      <div style={{ padding: "5px" }}></div>
-      {loader &&
-        Object.entries(formValue && formValue[0]["formData"]).map(
-          ([key, value]: [string, any], index: number) => (
-            <FeedbackContainer>
-              <FeedbackItem>
-                <span>
-                  {`Question ${index + 1} .`}
-                  <b>{key}</b>
-                </span>
-                <div style={{ padding: "5px" }}></div>
-                <div>
-                  <b>{value.Description}</b>
-                </div>
-                <div style={{ padding: "5px" }}></div>
-                <div>
-                  <Radio.Group onChange={handleChange} name={key}>
-                    <div>
-                      {value.Options.map((option: any) => (
-                        <Radio value={option.value}>
-                          <div>{option.value}</div>
-                        </Radio>
-                      ))}
-                    </div>
-                  </Radio.Group>
-                </div>
-              </FeedbackItem>
-            </FeedbackContainer>
-          )
-        )}
+          <div style={{ padding: "5px" }}></div>
+          {Object.entries(formValue && formValue[0]["formData"]).map(
+            ([key, value]: [string, any], index: number) => (
+              <FeedbackContainer>
+                <FeedbackItem>
+                  <span>
+                    {`Question ${index + 1} .`}
+                    <b>{key}</b>
+                  </span>
+                  <div style={{ padding: "5px" }}></div>
+                  <div>
+                    <b>{value.Description}</b>
+                  </div>
+                  <div style={{ padding: "5px" }}></div>
+                  <div>
+                    <Radio.Group onChange={handleChange} name={key}>
+                      <div>
+                        {value.Options.map((option: any) => (
+                          <Radio value={option.value}>
+                            <div>{option.value}</div>
+                          </Radio>
+                        ))}
+                      </div>
+                    </Radio.Group>
+                  </div>
+                </FeedbackItem>
+              </FeedbackContainer>
+            )
+          )}
+        </>
+      )}
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Button type="primary" onClick={handleSubmit}>
           Submit
